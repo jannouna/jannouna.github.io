101d8091ea983c22aa7c1736f41e2f0361bc926b
jannouna <107442603+jannouna@users.noreply.github.com>
2026-08-24 14:27:25 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,1 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://jannouna.github.io',
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
